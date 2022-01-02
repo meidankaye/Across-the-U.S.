26b51323be5378f6ae2b476e8002b0a4f5649ed0
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-01-02 22:17:16 +0200

    feat: card-prev

--- scripts/index.js
+++ scripts/index.js
@@ -1,96 +1,116 @@
-const initialCards = [
-    {
-      name: "Yosemite Valley",
-      link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+const initialCards = [{
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
     },
     {
-      name: "Lake Louise",
-      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
     },
     {
-      name: "Bald Mountains",
-      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
     },
     {
-      name: "Latemar",
-      link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg",
     },
     {
-      name: "Vanoise National Park",
-      link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+        name: "Vanoise National Park",
+        link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
     },
     {
-      name: "Lago di Braies",
-      link: "https://code.s3.yandex.net/web-code/lago.jpg"
-    }
-  ];
-  
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg",
+    },
+];
+
 /* -------------------------------------------------------------------------- */
 /*                                  Wrappers                                  */
 /* -------------------------------------------------------------------------- */
 
-const popup = document.querySelector('.popup');
-const editForm = document.querySelector('.popup__form');
-const places = document.querySelector('.places');
+const editPopup = document.querySelector(".popup_type_edit");
+const addPopup = document.querySelector(".popup_type_add");
+const editForm = editPopup.querySelector(".popup__form");
+const addForm = addPopup.querySelector(".popup__form");
+const places = document.querySelector(".places");
 
 /* -------------------------------------------------------------------------- */
 /*                            Buttons and DOM nodes                           */
 /* -------------------------------------------------------------------------- */
 
-const profileName = document.querySelector('.profile__name');
-const profileProfession = document.querySelector('.profile__profession');
-const editBtn = document.querySelector('.profile__edit-button');
-const closeBtn = document.querySelector('.popup__close-button');
+const profileName = document.querySelector(".profile__name");
+const profileProfession = document.querySelector(".profile__profession");
+const editBtn = document.querySelector(".profile__edit-button");
+const addBtn = document.querySelector(".profile__add-button");
+const editPopupCloseBtn = editPopup.querySelector(".popup__close-button");
+const addPopupCloseBtn = addPopup.querySelector(".popup__close-button");
 
 /* -------------------------------------------------------------------------- */
 /*                                  Inputs                                    */
 /* -------------------------------------------------------------------------- */
 
-const popupInputName = editForm.querySelector('.popup__input_text_name');
-const popupInputProfession = editForm.querySelector('.popup__input_text_profession');
+const popupInputName = editForm.querySelector(".popup__input_text_name");
+const popupInputProfession = editForm.querySelector(
+    ".popup__input_text_profession"
+);
+const popupInputTitle = addForm.querySelector(".popup__input_text_title");
+const popupInputLink = addForm.querySelector(".popup__input_text_link");
 
 /* -------------------------------------------------------------------------- */
 /*                                  Templates                                 */
 /* -------------------------------------------------------------------------- */
 
-const placesTemplate = document.querySelector('#place-template')
-.content.querySelector('.places__item');
-
+const placesTemplate = document
+    .querySelector("#place-template")
+    .content.querySelector(".places__item");
 
 /* -------------------------------------------------------------------------- */
 /*                               Event Listeners                              */
 /* -------------------------------------------------------------------------- */
 
-editBtn.addEventListener('click', () => {
+editBtn.addEventListener("click", () => {
     popupInputName.value = profileName.textContent;
     popupInputProfession.value = profileProfession.textContent;
-    togglePopup();
+    togglePopup(editPopup);
 });
 
-editForm.addEventListener('submit', (e) => {
+editForm.addEventListener("submit", (e) => {
     profileName.textContent = popupInputName.value;
     profileProfession.textContent = popupInputProfession.value;
-    togglePopup();
+    togglePopup(editPopup);
     e.preventDefault();
 });
 
-closeBtn.addEventListener('click', togglePopup);
+editPopupCloseBtn.addEventListener("click", () => {
+    togglePopup(editPopup);
+});
 
+addBtn.addEventListener("click", () => togglePopup(addPopup));
+
+addPopupCloseBtn.addEventListener("click", () => {
+    togglePopup(addPopup);
+});
 
 /* -------------------------------------------------------------------------- */
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
 
-function togglePopup() {
-    popup.classList.toggle('popup_opened');
+function togglePopup(popup) {
+    popup.classList.toggle("popup_opened");
 }
 
 function createPlace(card) {
     const placeElement = placesTemplate.cloneNode(true);
 
-    placeElement.querySelector('.place__title').textContent = card.name;
+    placeElement.querySelector(".place__title").textContent = card.name;
 
-    placeElement.querySelector('.place__image').style.backgroundImage = `url(${card.link})`;
+    const imageElem = placeElement.querySelector(
+        ".place__image"
+    );
+    imageElem.style.backgroundImage = `url(${card.link})`;
+    imageElem.addEventListener("click", function() {
+        togglePopup();
+    })
 
     return placeElement;
 }
@@ -99,7 +119,7 @@ function renderPlace(card, container) {
     container.append(card);
 }
 
-initialCards.forEach(function (card) {
+initialCards.forEach(function(card) {
     const newPlace = createPlace(card);
     renderPlace(newPlace, places);
 });
