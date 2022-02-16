4c4defbe1089205551a98742c11cbba5b674d963
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-02-16 21:59:15 +0200

    feat: babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    devtool: 'inline-source-map',
-    entry: {
-      main: "./src/index.js"
-    },
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "main.js",
-      publicPath: ""
-    },
-    mode: 'development',
-    devServer: {
-      static: path.resolve(__dirname, './dist'),
-      compress: true,
-      port: 8080,
-      open: true
-    },
+  devtool: 'inline-source-map',
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: ''
+  },
+  target: ['web', 'es5'],
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/"
+      }
+    ]
   }
+}
