679ebddd6dbccb7c55e22ecf2c6b1fffa75f4426
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-02-16 23:09:27 +0200

    feat: almost done webpack

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,34 @@
+import yosemite from "https://code.s3.yandex.net/web-code/yosemite.jpg"
+import louise from "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+import baldMt from "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+import latemar from "https://code.s3.yandex.net/web-code/latemar.jpg"
+import vanoise from "https://code.s3.yandex.net/web-code/vanoise.jpg"
+import lago from "https://code.s3.yandex.net/web-code/lago.jpg"
+
+
 const initialCards = [{
     name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+    link: yosemite,
 },
 {
     name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+    link: louise,
 },
 {
     name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+    link: baldMt,
 },
 {
     name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+    link: latemar,
 },
 {
     name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+    link: vanoise,
 },
 {
     name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+    link: lago,
 },
 ];
 
