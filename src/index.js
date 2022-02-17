a4432124ce036fca809e5ad5424ae53c7770602d
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-02-17 21:20:44 +0200

    fix: webpack working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
-import "../page/index.css";
+import "./index.css";
 import Card from "../scripts/Card.js";
 import FormValidator from "../scripts/FormValidator.js";
 import { openPopup, closePopup } from "../scripts/utils.js";
+
+
+const initialCards = [{
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+},
+{
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+},
+{
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+},
+{
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+},
+{
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+},
+{
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+},
+];
+
+initialCards.reverse();
 
 /* -------------------------------------------------------------------------- */
 /*                                  Popups                                    */
